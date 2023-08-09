55e2918be781795f95b472cf786b7c5c56a23522
Aditya <Aditya@DESKTOP-5J8IC55>
2023-08-09 00:20:03 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1
+{
+    internal class Class1
+    {
+        String[] names = new string[4];
+        public void inputData()
+        {
+            Console.WriteLine("enter members names:");
+            for(int i = 0;i <4; i++)
+            {
+                Console.WriteLine("enter name of member{0}", (i + 1));
+                names[i]=Console.ReadLine();
+            }
+        }
+        public void showData()
+        {
+            Console.WriteLine("enter members names:");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("enter name of member{0} is {1}", (i + 1), names[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello world");
+            Class1 MCA = new Class1();
+            MCA.inputData();
+            MCA.showData();
+            System.Console.ReadKey();
+        }
+    }
+}
